6bc77e8025cf0400b3924ba7a3bc4e728b4f76ef
JoaoAlexNunes <jalnunes.2002@gmail.com>
2023-12-30 13:19:14 +0000

    Added ocamlformat and formated the code

--- Ocaml/restapi/bin/main.ml
+++ Ocaml/restapi/bin/main.ml
@@ -1,6 +1,10 @@
-let routes = 
-  Dream.([get "/hello" (fun (_:request) -> html  @@ Restapi.greetings ^ "world! \n");
-          get "/hello/:name" (fun request -> html @@ Restapi.greetings ^ (Dream.param request "name") ^"! \n")])
+let routes =
+  Dream.
+    [
+      get "/hello" (fun (_ : request) ->
+          html @@ Restapi.greetings ^ "world! \n");
+      get "/hello/:name" (fun request ->
+          html @@ Restapi.greetings ^ Dream.param request "name" ^ "! \n");
+    ]
 
-let () =
-  Dream.(run (router routes))
+let () = Dream.(run (router routes))
