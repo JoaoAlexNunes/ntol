cc395318ee70d9ce7575c762c5e0561db00c3185
JoaoAlexNunes <jalnunes.2002@gmail.com>
2023-12-30 01:04:55 +0000

    Added restapi Ocaml

---
+++ Ocaml/restapi/_build/default/bin/main.ml
@@ -0,0 +1,6 @@
+let routes = 
+  Dream.([get "/hello" (fun (_:request) -> html  @@ Restapi.greetings ^ "world! \n");
+          get "/hello/:name" (fun request -> html @@ Restapi.greetings ^ (Dream.param request "name") ^"! \n")])
+
+let () =
+  Dream.(run (router routes))
