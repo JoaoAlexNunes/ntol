0354fe3e8913a79337819c1f7e0255cea2632d85
JoaoAlexNunes <jalnunes.2002@gmail.com>
2023-12-31 00:44:17 +0000

    Ocaml: Rename restapi to hello_world and mv to snippets

--- Ocaml/snippets/hello_world/bin/main.ml
+++ Ocaml/snippets/hello_world/bin/main.ml
@@ -1,17 +1,17 @@
 let text_plain = ("Content-type:", "text/plain")
 
 (* GET `/hello` returns text response of "Hello World!" *)
-let hello _ = Dream.respond ~headers:[ text_plain ] Restapi.greetings
+let hello _ = Dream.respond ~headers:[ text_plain ] Hello_world.greetings
 
 (* GET `/hello/<name>` returns json response of `{"greeting": "Hello <name>!"}` *)
 let hello_name req =
-  Dream.json @@ Restapi.get_greeting @@ Dream.param req "name"
+  Dream.json @@ Hello_world.get_greeting @@ Dream.param req "name"
 
 (* POST `/goodbye` takes a json payload of `{"name":string}` and returns text response of "Goodbye <name>" *)
 let goodbye req =
   let%lwt body = Dream.body req in
 
-  match Restapi.get_name body with
+  match Hello_world.get_name body with
   | Ok n -> Dream.respond ~headers:[ text_plain ] @@ "Goodbye " ^ n
   | Error _ -> Dream.respond ~status:`Bad_Request @@ "Bad Request: " ^ body
 
